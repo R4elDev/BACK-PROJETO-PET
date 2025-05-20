154368958413494bf8d877befeb289cc3b0bd4a2
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@dsn127m1249386.cfp127aluno.local>
2025-05-20 16:15:53 -0300

    adicionando pastas principais

---
+++ database/script.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE db_projeto_adocao_pets_bb;
+
+use db_projeto_adocao_pets_bb;
+
+CREATE TABLE tbl_cadastro_tutor(
+	id int not null primary key auto_increment,
+	nome VARCHAR(100) not null,
+    email VARCHAR(150) not null,
+    endereco VARCHAR(200) not null,
+    cnpj VARCHAR(45) not null,
+    data_nascimento DATE not null
+);
+
+CREATE TABLE tbl_cadastro_ong(
+	id int not null primary key auto_increment,
+    nome VARCHAR(100),
+    email VARCHAR(150) not null,
+    regiao VARCHAR(100) not null,
+    endereco VARCHAR(200) not null
+);
