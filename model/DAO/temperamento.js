d327f43f02895d7910460b01954f04d3dfd4f452
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@dsn127m1241954.cfp127aluno.local>
2025-06-03 16:36:46 -0300

    Realizado a model de Temperamento

--- model/DAO/temperamento.js
+++ model/DAO/temperamento.js
@@ -1,5 +1,5 @@
 /*****************************************************************************************
- * Objetivo --> Model responsavel pelo CRUD de dados referente a vacina no Banco de dados
+ * Objetivo --> Model responsavel pelo CRUD de dados referente a temperamento no Banco de dados
  * Data --> 03/06/2025
  * Autor --> Israel
  * Versão --> 1.0
@@ -11,18 +11,18 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 // Função para inserir uma vacina no banco de dados
-const insertVacina = async function (vacina){
+const insertTemperamento = async function (temperamento){
     try{
         
-        let sql = `INSERT INTO tbl_vacina(nome_vacina)
+        let sql = `INSERT INTO tbl_temperamento(nome_temperamento)
     values(
-        '${vacina.nome_vacina}'
+        '${temperamento.nome_temperamento}'
     );`
 
         let result = await prisma.$executeRawUnsafe(sql)
 
         if(result){
-            let sqlSelect = `SELECT * FROM tbl_vacina WHERE nome_vacina = '${vacina.nome_vacina}' ORDER BY id DESC LIMIT 1`
+            let sqlSelect = `SELECT * FROM tbl_temperamento WHERE nome_temperamento = '${temperamento.nome_temperamento}' ORDER BY id DESC LIMIT 1`
 
             let scriptCriado = await prisma.$queryRawUnsafe(sqlSelect)
             return scriptCriado[0]
@@ -35,10 +35,10 @@ const insertVacina = async function (vacina){
     }
 }
 
-const updateVacina = async function (vacina){
+const updateTemperamento = async function (temperamento){
     try{
-        let sql = `UPDATE tbl_vacina set nome = '${vacina.nome_vacina}'
-                                                 where id = ${vacina.id}`
+        let sql = `UPDATE tbl_temperamento set nome = '${temperamento.nome_temperamento}'
+                                                 where id = ${temperamento.id}`
 
         let result = await prisma.$executeRawUnsafe(sql)
 
@@ -52,11 +52,11 @@ const updateVacina = async function (vacina){
     }
 }
 
-const deleteVacina = async function (id){
+const deleteTemperamento = async function (id){
     try{
-        let idVacina = id
+        let idTemperamento = id
 
-        let sql = `DELETE FROM tbl_vacina WHERE id=${idVacina}`
+        let sql = `DELETE FROM tbl_temperamento WHERE id=${idTemperamento}`
 
         let result = await prisma.$executeRawUnsafe(sql)
 
@@ -70,9 +70,9 @@ const deleteVacina = async function (id){
     }
 }
 
-const selectAllVacina = async function (){
+const selectAllTemperamento = async function (){
     try{
-        let sql = `SELECT * FROM tbl_vacina`
+        let sql = `SELECT * FROM tbl_temperamento`
 
         let result = await prisma.$queryRawUnsafe(sql)
 
@@ -88,11 +88,11 @@ const selectAllVacina = async function (){
     }
 }
 
-const selectByIdVacina = async function (id){
+const selectByIdTemperamento= async function (id){
     try{
-        let idVacina = id
+        let idTemperamento = id
 
-        let sql = `SELECT * FROM tbl_vacina WHERE id=${idVacina}`
+        let sql = `SELECT * FROM tbl_temperamento WHERE id=${idTemperamento}`
 
         let result = await prisma.$queryRawUnsafe(sql)
 
@@ -109,8 +109,9 @@ const selectByIdVacina = async function (id){
 
 
 module.exports = {
-    insertVacina,
-    updateVacina,
-    selectAllVacina,
-    selectByIdVacina
+   insertTemperamento,
+   updateTemperamento,
+   selectAllTemperamento,
+   selectByIdTemperamento,
+   deleteTemperamento
 }
